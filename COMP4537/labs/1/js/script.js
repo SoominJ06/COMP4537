eb1ec7d081c90b377b122af57157ca9364da910d
Soomin Jeong <sjeong0643@gmail.com>
2025-01-18 00:50:23 -0800

    Separating string const variables

--- COMP4537/labs/1/js/script.js
+++ COMP4537/labs/1/js/script.js
@@ -1,18 +1,62 @@
+// Id names and classnames
+const readerNoteClass = "readerNote";
+const readerNoteContentClass = "readerNoteContent";
+const writerNoteClass = "writerNote";
+const writerTextareaClass = "writerTextarea"
+const noteBtnClass = "noteBtn";
+
+// Element names
+const createButtonElement = "button";
+const createDivElement = "div";
+const createTextareaElement = "textarea";
+
+// Attributes
+const underscoreDdlimiter = "_";
+const emptyString = "";
+const htmlPage = ".html";
+const emptyObj = "[]";
+
+// Names of elements in document
+const grabNotesWrap = "notesWrap";
+const grabTimeDisplay = "timeDisplay;"
+const grabUpdatedTime = "updatedTime";
+const grabAddBtn = "addBtn";
+const grabTitle = "title";
+const grabName = "name";
+const grabIndexNav = "toIndex";
+const grabWriterNav = "toWriter";
+const grabReaderNav = "toReader";
+
+// Event names
+const clickEvent = "click";
+const storageEvent = "storage";
+const inputEvent = "input";
+const domContentLoadedEvent = "DOMContentLoaded";
+
+// localStorage key names
+const storageUserNotes = "user_notes";
+const lastSavedTime = "last_saved_time";
+
+// Page names
+const indexPage = "index";
+const writerPage = "writer";
+const readerPage = "reader";
+
 class ReaderNotes {
     constructor(notes) {
         this.notes = notes;
         this.time = this.getSavedTime();
         this.loadAllNotes();
         // ChatGPT suggested using storage event listener to update the notes in real-time
-        window.addEventListener("storage", (event) => {
-            if (event.key === "user_notes") {
+        window.addEventListener(storageEvent, (event) => {
+            if (event.key === storageUserNotes) {
                 this.updateNotes(); // Reload notes when user_notes changes
             }
         });
     }
 
     clearAllNotes() {
-        document.getElementById("notesWrap").innerHTML = "";
+        document.getElementById(grabNotesWrap).innerHTML = emptyString;
     }
 
     loadAllNotes() {
@@ -20,7 +64,7 @@ class ReaderNotes {
         const existingNotes = this.notes.getAllNotes();
         existingNotes.forEach((note) => {
             const noteElement = this.createReaderNote(note);
-            document.getElementById("notesWrap").appendChild(noteElement);
+            document.getElementById(grabNotesWrap).appendChild(noteElement);
         });
         this.insertTime(this.time); // Display the saved time (if any)
     }
@@ -31,17 +75,17 @@ class ReaderNotes {
     }
 
     createReaderNote(content) {
-        const note = document.createElement("div");
-        note.classList.add("readerNote");
+        const note = document.createElement(createDivElement);
+        note.classList.add(readerNoteClass);
         note.appendChild(this.createNoteContent(content))
         this.updateTime();
         return note;
     }
 
     createNoteContent(content) {
-        const noteContent = document.createElement("div");
-        noteContent.classList.add("readerNoteContent");
-        noteContent.innerHTML = content ? content : "";
+        const noteContent = document.createElement(createDivElement);
+        noteContent.classList.add(readerNoteContentClass);
+        noteContent.innerHTML = content ? content : emptyString;
         return noteContent;
     }
 
@@ -54,17 +98,17 @@ class ReaderNotes {
     }
 
     getSavedTime() {
-        return localStorage.getItem("last_saved_time") || "";
+        return localStorage.getItem(lastSavedTime) || emptyString;
     }
 
     saveTime(time) {
-        localStorage.setItem("last_saved_time", time);
+        localStorage.setItem(lastSavedTime, time);
     }
 
     insertTime(time) {
-        document.getElementById("timeDisplay").innerHTML = messages.updatedTime;
-        const timeElement = document.getElementById("updatedTime");
-        timeElement.innerHTML = timeElement ? time : "";
+        document.getElementById(grabTimeDisplay).innerHTML = messages.updatedTime;
+        const timeElement = document.getElementById(grabUpdatedTime);
+        timeElement.innerHTML = timeElement ? time : emptyString;
     }
 }
 
@@ -73,47 +117,47 @@ class WriterNotes {
         this.notes = notes;
         this.time = this.getSavedTime();
         this.loadExistingNotes();
-        document.getElementById("addBtn").innerHTML = messages.addBtn;
+        document.getElementById(grabAddBtn).innerHTML = messages.addBtn;
     }
 
     loadExistingNotes() {
         const existingNotes = this.notes.getAllNotes();
         existingNotes.forEach((note, index) => {
             const noteElement = this.createWriterNote(note, index);
-            document.getElementById("notesWrap").appendChild(noteElement);
+            document.getElementById(grabNotesWrap).appendChild(noteElement);
         });
         this.insertTime(this.time); // Display the saved time (if any)
     }
 
     createWriterNote(content, index) {
-        const newNote = document.createElement("div");
-        newNote.classList.add("writerNote");
-        newNote.id = `writerNote_${index}`;
+        const newNote = document.createElement(createDivElement);
+        newNote.classList.add(writerNoteClass);
+        newNote.id = writerNoteClass + underscoreDdlimiter + index;
         newNote.appendChild(this.createTextarea(content));
         newNote.appendChild(this.createRemoveBtn());
         return newNote;
     }
 
     createTextarea(content) {
-        const newTextarea = document.createElement("textarea");
-        newTextarea.classList.add("writerTextarea");
-        newTextarea.value = content ? content : "";
-        newTextarea.addEventListener("input", () => this.handleNoteChange(newTextarea));
+        const newTextarea = document.createElement(createTextareaElement);
+        newTextarea.classList.add(writerTextareaClass);
+        newTextarea.value = content ? content : emptyString;
+        newTextarea.addEventListener(inputEvent, () => this.handleNoteChange(newTextarea));
         return newTextarea;
     }
 
     createRemoveBtn() {
-        const newRemoveBtn = document.createElement("button");
-        newRemoveBtn.classList.add("noteBtn");
+        const newRemoveBtn = document.createElement(createButtonElement);
+        newRemoveBtn.classList.add(noteBtnClass);
         newRemoveBtn.innerHTML = messages.removeBtn;
-        newRemoveBtn.addEventListener("click", () => this.deleteNote(newRemoveBtn));
+        newRemoveBtn.addEventListener(clickEvent, () => this.deleteNote(newRemoveBtn));
         return newRemoveBtn;
     }
 
     deleteNote(removeBtn) {
-        const noteIndex = parseInt(removeBtn.parentElement.id.split("_")[1], 10);
+        const noteIndex = parseInt(removeBtn.parentElement.id.split(underscoreDdlimiter)[1], 10);
         this.notes.removeFromNotes(noteIndex);
-        document.getElementById("notesWrap").innerHTML = ""; // Clear existing notes
+        document.getElementById(grabNotesWrap).innerHTML = emptyString; // Clear existing notes
         this.updateTime();
         this.loadExistingNotes(); // Reinitialize notes
     }
@@ -121,14 +165,14 @@ class WriterNotes {
     createNewNote(e) {
         e.preventDefault();
         const newNoteIndex = this.notes.getAllNotes().length;
-        this.notes.addToNotes("");
-        const newNote = this.createWriterNote("", newNoteIndex);
-        document.getElementById("notesWrap").appendChild(newNote);
+        this.notes.addToNotes(emptyString);
+        const newNote = this.createWriterNote(emptyString, newNoteIndex);
+        document.getElementById(grabNotesWrap).appendChild(newNote);
         this.updateTime();
     }
     
     handleNoteChange(textarea) {
-        const noteIndex = parseInt(textarea.parentElement.id.split("_")[1], 10);
+        const noteIndex = parseInt(textarea.parentElement.id.split(underscoreDdlimiter)[1], 10);
         this.updateTime();
         this.notes.updateNote(noteIndex, textarea.value);
     }
@@ -142,47 +186,47 @@ class WriterNotes {
     }
 
     getSavedTime() {
-        return localStorage.getItem("last_saved_time") || "";
+        return localStorage.getItem(lastSavedTime) || emptyString;
     }
 
     saveTime(time) {
-        localStorage.setItem("last_saved_time", time);
+        localStorage.setItem(lastSavedTime, time);
     }
 
     insertTime(time) {
-        document.getElementById("timeDisplay").innerHTML = messages.updatedTime;
-        const timeElement = document.getElementById("updatedTime");
-        timeElement.innerHTML = timeElement ? time : "";
+        document.getElementById(grabTimeDisplay).innerHTML = messages.updatedTime;
+        const timeElement = document.getElementById(grabUpdatedTime);
+        timeElement.innerHTML = timeElement ? time : emptyString;
     }
 }
 
 class NoteStorage {
     constructor() {
-        this.notes = localStorage.getItem("user_notes") || "[]";
+        this.notes = localStorage.getItem(storageUserNotes) || emptyObj;
     }
 
     getAllNotes() {
-        return JSON.parse(localStorage.getItem("user_notes") || "[]");
+        return JSON.parse(localStorage.getItem(storageUserNotes) || emptyObj);
     }
 
     addToNotes(note) {
         const notes = this.getAllNotes();
         notes.push(note);
-        localStorage.setItem("user_notes", JSON.stringify(notes));
+        localStorage.setItem(storageUserNotes, JSON.stringify(notes));
     }
 
     updateNote(index, content) {
         const notes = this.getAllNotes();
         if (index >= 0 && index < notes.length) {
             notes[index] = content;
-            localStorage.setItem("user_notes", JSON.stringify(notes));
+            localStorage.setItem(storageUserNotes, JSON.stringify(notes));
         }
     }
 
     removeFromNotes(index) {
         const notes = this.getAllNotes();
         notes.splice(index, 1);
-        localStorage.setItem("user_notes", JSON.stringify(notes));
+        localStorage.setItem(storageUserNotes, JSON.stringify(notes));
     }
 }
 
@@ -195,35 +239,35 @@ class UI {
     // Initializes UI with corresponding page
     init(currLocation) {
         const currPage = currLocation.pathname;
-        if (currPage.includes("index")) {
+        if (currPage.includes(indexPage)) {
             this.initIndex();
-        } else if (currPage.includes("writer")) {
+        } else if (currPage.includes(writerPage)) {
             this.initWriter();
-        } else if (currPage.includes("reader")) {
+        } else if (currPage.includes(readerPage)) {
             this.initReader();
         }
     }
 
     // Navigation initializations
     initNavToIndex() {
-        const toIndex = document.getElementById("toIndex");
+        const toIndex = document.getElementById(grabIndexNav);
         if (toIndex) {
             toIndex.innerHTML = messages.toIndexBtn;
-            toIndex.onclick = () => {window.location = `index.html`};
+            toIndex.onclick = () => {window.location = indexPage + htmlPage};
         }
     }
     initNavToWriter() {
-        const toWriter = document.getElementById("toWriter");
+        const toWriter = document.getElementById(grabWriterNav);
         if (toWriter) {
             toWriter.innerHTML = messages.toWriterBtn;
-            toWriter.onclick = () => {window.location = `writer.html`};
+            toWriter.onclick = () => {window.location = writerPage + htmlPage};
         }
     }
     initNavToReader() {
-        const toReader = document.getElementById("toReader");
+        const toReader = document.getElementById(grabReaderNav);
         if (toReader) {
             toReader.innerHTML = messages.toReaderBtn;
-            toReader.onclick = () => {window.location = `reader.html`};
+            toReader.onclick = () => {window.location = readerPage + htmlPage};
         }
     }
 
@@ -231,22 +275,22 @@ class UI {
     initIndex() {
         this.initNavToWriter();
         this.initNavToReader();
-        document.getElementById("title").innerHTML = messages.indexTitle;
-        document.getElementById("name").innerHTML = messages.name;
+        document.getElementById(grabTitle).innerHTML = messages.indexTitle;
+        document.getElementById(grabName).innerHTML = messages.name;
     }
     initWriter() {
         this.initNavToIndex();
-        document.getElementById("title").innerHTML = messages.writerTitle;
+        document.getElementById(grabTitle).innerHTML = messages.writerTitle;
         const writerNotes = new WriterNotes(this.notes);
-        document.getElementById("addBtn").addEventListener("click", (e) => writerNotes.createNewNote(e)); 
+        document.getElementById(grabAddBtn).addEventListener(clickEvent, (e) => writerNotes.createNewNote(e)); 
     }
     initReader() {
         this.initNavToIndex();
-        document.getElementById("title").innerHTML = messages.readerTitle;
+        document.getElementById(grabTitle).innerHTML = messages.readerTitle;
         new ReaderNotes(this.notes);
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener(domContentLoadedEvent, () => {
     new UI(window.location);
 });
